7f07d1a5dd7a29271f37a177e0c2c59ecf56de7d
lucas.fiori <lucas.fiori@usp.br>
2021-06-15 16:07:29 -0300

    Update itemA

--- EP2/itemA.m
+++ EP2/itemA.m
@@ -1,8 +1,10 @@
-function []=ItemA(Az, dx, dy)
+function [Az] = ItemA(Az, dx, dy)
 %Input:
-%   i  =
+%   Az =
+%   dx =
+%   dy = 
 %Output:
-%   Jz_val = 
+%   Az = 
 
 mi_0 = 4*pi*10^-7;
 mi_ferro = 2500*mi_0;
@@ -15,12 +17,17 @@ lambda = 1.75;
 E = 0.0001;
 
 while iter == true
-    highest_E = 0
+    highest_E = 0;
     for j = 1:(10/dy)
         for i = 2:(22/dx)
-            Az_old = Az(j, i)
+            Az_old = Az(j, i);
+            
+            % Caso o ponto pertenca ao contorno
+            if(i*dx == 0 || i*dx == 22 || j*dy == 10 || (i*dx == 20 &&(j*dy >= 6 && j < 10) || (i*dx > 20 && i*dx < 22 && j*dy == 6)))
+                continue
+                
             % Caso o ponto pertenca a bobina
-            if(((i*dx > 14 && i*dx < 16) || (i*dx > 20 && i*dx < 22)) && (j*dy < 6))
+            elseif(((i*dx > 14 && i*dx < 16) || (i*dx > 20 && i*dx < 22)) && (j*dy < 6))
                 if(j == 1)
                     Az(j,i) = (2*Az(j+1, i) + Az(j, i+1) + Az(j, i-1) + (dx^2)*mi_b*(2*(10^6)*cos((pi*i/dy)/0.12) + 8*10^5)) / 4;
                 else
@@ -30,17 +37,17 @@ while iter == true
             elseif(i*dx == 4)
                 % Fronteira vertical ferro-ar
                 if(j == 1)
-                    Az(j, i) = (1/4*((mi_ar/mi_ferro) + 1))*(((mi_ar/mi_ferro) + 1)*2*Az(j+1, i) + 2*(Az(j, i+1) + (mi_ar/mi_ferro)*Az(j, i-1)));
+                    Az(j, i) = (1/(4*((mi_ar/mi_ferro) + 1)))*(((mi_ar/mi_ferro) + 1)*2*Az(j+1, i) + 2*(Az(j, i+1) + (mi_ar/mi_ferro)*Az(j, i-1)));
                 else
-                    Az(j, i) = (1/4*((mi_ar/mi_ferro) + 1))*(((mi_ar/mi_ferro) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ar/mi_ferro)*Az(j, i-1)));
+                    Az(j, i) = (1/(4*((mi_ar/mi_ferro) + 1)))*(((mi_ar/mi_ferro) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ar/mi_ferro)*Az(j, i-1)));
                 end
  
             elseif(i*dx == 5 && j*dy >= 6)
                 % Fronteira vertical ar-ferro
-                if(j == 6/deltax)
-                    Az(j, i) = (1/(2*((mi_ferro/mi_ar) + 1))) * ((mi_ferro/mi_ar)*(Az(j-1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
+                if(j*dy == 6)
+                    Az(j, i) = (1/(4*((mi_ferro/mi_ar) + 1))) * ((mi_ferro/mi_ar)*(Az(j-1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
                 else 
-                    Az(j, i) = (1/4*((mi_ferro/mi_ar) + 1))*(((mi_ferro/mi_ar) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ferro/mi_ar)*Az(j, i-1)));
+                    Az(j, i) = (1/(4*((mi_ferro/mi_ar) + 1)))*(((mi_ferro/mi_ar) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ferro/mi_ar)*Az(j, i-1)));
                 end
 
             elseif((i*dx > 5 && i*dx < 14) && j*dy == 6)
@@ -58,28 +65,28 @@ while iter == true
             elseif((i*dx == 14 && j*dy < 6))
                 % Fronteira vertical ar-bobina
                 if (j == 1)
-                    Az(j, i) = (1/(2*((mi_b/mi_ar) + 1))) * ((mi_b/mi_ar)*(Az(j-1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
+                    Az(j, i) = (1/(4*((mi_b/mi_ar) + 1))) * ((mi_b/mi_ar)*(Az(j+1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
                 else 
-                    Az(j, i) = (1/4*((mi_b/mi_ar) + 1))*(((mi_b/mi_ar) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_b/mi_ar)*Az(j, i-1)));
+                    Az(j, i) = (1/(4*((mi_b/mi_ar) + 1)))*(((mi_b/mi_ar) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_b/mi_ar)*Az(j, i-1)));
                 end
 
             elseif((i*dx == 16 && j*dy < 6))
                 % Fronteira vertical bobina-ferro
                 if (j == 1)
-                    Az(j, i) = (1/(2*((mi_ferro/mi_b) + 1))) * ((mi_ferro/mi_b)*(Az(j-1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
+                    Az(j, i) = (1/(4*((mi_ferro/mi_b) + 1))) * ((mi_ferro/mi_b)*(Az(j+1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
                 else 
-                    Az(j, i) = (1/4*((mi_ferro/mi_b) + 1))*(((mi_ferro/mi_b) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ferro/mi_b)*Az(j, i-1)));
+                    Az(j, i) = (1/(4*((mi_ferro/mi_b) + 1)))*(((mi_ferro/mi_b) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_ferro/mi_b)*Az(j, i-1)));
                 end
             
             elseif((i*dx == 20 && j*dy < 6))
                 % Fronteira vertical ferro-bobina
                 if (j == 1)
-                    Az(j, i) = (1/(2*((mi_b/mi_ferro) + 1))) * ((mi_b/mi_ferro)*(Az(j-1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
+                    Az(j, i) = (1/(2*((mi_b/mi_ferro) + 1))) * ((mi_b/mi_ferro)*(Az(j+1, i) + Az(j, i-1)) + Az(j+1, i) + Az(j, i+1));
                 else 
                     Az(j, i) = (1/4*((mi_b/mi_ferro) + 1))*(((mi_b/mi_ferro) + 1)*(Az(j+1, i) + Az(j-1, i)) + 2*(Az(j, i+1) + (mi_b/mi_ferro)*Az(j, i-1)));
                 end
             
-                % Caso seja um ponto interno do domínio sem ser fronteira ou bobina (a maioria dos pontos)
+                % Caso seja um ponto interno do dominio sem ser fronteira ou bobina (a maioria dos pontos)
             else
                 if(j == 1)
                     Az(j, i) = (2*Az(j+1, i) + Az(j, i+1) + Az(j, i-1))/4;
@@ -89,10 +96,12 @@ while iter == true
             end
             
             Az(j,i) = lambda*Az(j,i) + (1-lambda)*Az_old;
-            % Armazena o maior valor do erro em módulo
+            % Armazena o maior valor do erro em modulo
             current_E = abs(Az(j,i) - Az_old);
             if (current_E > highest_E)
-                highest_E = current_E;
+                j
+                i
+                highest_E = current_E
             end 
 
         end
@@ -106,7 +115,7 @@ while iter == true
 
 end
 
-display('Iterações: ')
-contiteracoes
+display('Iteracoes: ')
+count
 
 end
---
+++ EP2/main.m
@@ -0,0 +1,18 @@
+close all; clear all; clc
+
+x = 22;
+y = 10;
+
+dx = 0.5;
+dy = 0.5;
+
+Az = zeros(y/dy, x/dx);
+
+Az_new = itemA(Az, dx, dy);
+
+h = heatmap(Az_new, 'xlabel', 'x', 'ylabel', 'y', 'Colormap', winter);
+
+h.GridVisible = 'off';
+Ax = gca;
+Ax.XDisplayLabels = nan(size(Ax.XDisplayData));
+Ax.YDisplayLabels = nan(size(Ax.YDisplayData));
