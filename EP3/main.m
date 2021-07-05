523f24ddfe9d72feaab3983d89ccfb4f17e036a4
lucas.fiori <lucas.fiori@usp.br>
2021-07-04 23:43:59 -0300

    Fix item a1, running but not working properly

--- EP3/main.m
+++ EP3/main.m
@@ -1,40 +1,46 @@
-A1 = 1.8*0.9;
-A2 = 0.9*0.9;
+b1 = 1.8; h1 = 0.9;
+A1 = b1*h1;
+b2 = 0.9; h2 = 0.9;
+A2 = b2*h2;
 D3 = 0.050;
+
+dx = 3;
 
 % Passo 1 - Definir a geometria (coordenadas e conectividades).
 
 if (dx == 1)
+	Nod_vh = size(0:dx:65, 2);
+	Nod_p = size(0:dx:23, 2);
+    
 	coords = [ 5  5;
        		  18  5;
-		  65  5;
-		  36 23;
-		  linspace(0, dx, 65)' 5;
-		  36 linspace(0, dx, 23)'];
-
-	Nod_vh = size(linspace(0, dx, 65));
-	Nod_p = size(linspace(0, dx, 23));
+              65  5;
+              36 23;
+              (0:dx:65)' 5*ones(Nod_vh, 1);
+              36*ones(Nod_p, 1) (0:dx:23)'];
 else
+	Nod_vh = size(0:dx:65, 2) + 1;
+	Nod_p = size(0:dx:23, 2) + 1;
+    
 	coords = [ 5  5;
        		  18  5;
-		  65  5;
-		  36 23;
-		  linspace(0, dx, 65)' 5;
-		  65 5
-		  36 linspace(0, dx, 23)'
-		  36 23];
+              65  5;
+              36 23;
+              (0:dx:65)' 5*ones(Nod_vh-1, 1);
+              65 5
+              36*ones(Nod_p-1, 1) (0:dx:23)'
+              36 23];
 
-	Nod_vh = size(linspace(0, dx, 65)) + 1;
-	Nod_p = size(linspace(0, dx, 23)) + 1;
 end
 
 	con = [ 1 4;
-		2 4;
-		3 4;
-		(5:(5 + Nod_vh - 2))' (6:(6 + Nod_vh - 2))';
-		((5 + Nod_vh):(5 + Nod_vh + Nod_p - 2))' ((6 + Nod_vh):(6 + Nod_vh + Nod_p - 2))'];
+            2 4;
+            3 4;
+            (5:(5 + Nod_vh - 2))' (6:(6 + Nod_vh - 2))';
+            ((5 + Nod_vh):(5 + Nod_vh + Nod_p - 2))' ((6 + Nod_vh):(6 + Nod_vh + Nod_p - 2))'];
 
-plot_struct(coords, con, '-b');
+%plot_struct(coords, con, '-b');
+
 
 % Passo 2 - Atribuir propriedades aos elementos.
 
@@ -47,10 +53,14 @@ Ngdl = 2*4 + 2*Nod_vh + 3*Nod_p;
 data.E = 210e9*ones(Nel, 1); % Vetor de modulos de Young dos elementos.
 data.rho = 7600*ones(Nel, 1); % Vetor de massa especifica dos elementos.
 data.A = ones(Nel, 1); % Vetor de areas dos elementos.
+data.I = ones(Nel, 1); % Vetor de momentos de inercia dos elementos.
 
 data.A(1:3) = pi*(D3/2)^2;
 data.A(4:(4 + Nel_vh - 1)) = A2;
 data.A((4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)) = A1;
+
+data.I(4:(4 + Nel_vh - 1)) = b2*((h2)^3)/12;
+data.I((4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)) = b1*((h1)^3)/12;
 
 data.L = zeros(Nel, 1); % Vetor de comprimento dos elementos.
 data.Q = zeros(Nel, 1); % Vetor de rotacoes dos elementos.
@@ -75,10 +85,10 @@ Mg = zeros(Ngdl, Ngdl);
 
 % Construcao das matrizes para os elementos de trelica
 
-k0 = [1  0 -1 0;
-      0  0  0 0;
+k0 = [ 1 0 -1 0;
+       0 0  0 0;
       -1 0  1 0;
-      0  0  0 0];
+       0 0  0 0];
 
 m0 = [2 0 1 0;
       0 0 0 0;
@@ -118,7 +128,7 @@ end
 
 % Construcao das matrizes para os elementos de viga horizontal.
 
-for e=4:(Nel_vh-1)
+for e=4:(4 + Nel_vh - 1)
 
 	k0 = [12           6*data.L(e)     -12           6*data.L(e);
 	      6*data.L(e)  4*(data.L(e))^2 -6*data.L(e)  2*(data.L(e))^2;
@@ -150,12 +160,12 @@ end
 
 % Construcao das matrizes para os elementos de portico (viga vertical).
 
-k0_t = [1  0 0 -1 0 0;
-        0  0 0  0 0 0;
-	0  0 0  0 0 0;
-	-1 0 0  1 0 0;
-	0  0 0  0 0 0;
-	0  0 0  0 0 0];
+k0_t = [ 1 0 0 -1 0 0;
+         0 0 0  0 0 0;
+	     0 0 0  0 0 0;
+	    -1 0 0  1 0 0;
+	     0 0 0  0 0 0;
+	     0 0 0  0 0 0];
 
 m0_t = [2 0 0 1 0 0;
         0 0 0 0 0 0;
@@ -164,23 +174,25 @@ m0_t = [2 0 0 1 0 0;
         0 0 0 0 0 0;
         0 0 0 0 0 0];
 
+v_ln = 5 + Nod_vh - 1;
+    
 for e=(4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)
 
 	ke_t = (data.E(e)*data.A(e)/data.L(e))*k0_t;
 	me_t = (data.rho(e)*data.A(e)*data.L(e)/6)*m0_t;
 
 	k0_v = [0  0           0               0  0            0;
-		0  12          6*data.L(e)     0 -12           6*data.L(e);
+		    0  12          6*data.L(e)     0 -12           6*data.L(e);
 	        0  6*data.L(e) 4*(data.L(e))^2 0 -6*data.L(e)  2*(data.L(e))^2;
 	        0  0           0               0  0            0;       
-		0 -12         -6*data.L(e)     0  12          -6*data.L(e);
+		    0 -12         -6*data.L(e)     0  12          -6*data.L(e);
 	        0  6*data.L(e) 2*(data.L(e))^2 0 -6*data.L(e)  4*(data.L(e))^2];
 
 	m0_v = [ 0  0              0                0 0              0;
-		 0  156            22*data.L(e)     0 54            -13*data.L(e);
+		     0  156            22*data.L(e)     0 54            -13*data.L(e);
 	         0  22*data.L(e)   4*(data.L(e))^2  0 13*data.L(e)  -3*(data.L(e))^2;
 	         0  0              0                0 0              0;       
-		 0  54             13*data.L(e)     0 156           -22*data.L(e);
+		     0  54             13*data.L(e)     0 156           -22*data.L(e);
 	         0 -13*data.L(e)  -3*(data.L(e))^2  0 -22*data.L(e)  4*(data.L(e))^2];
 
 	ke_v = data.E(e)*data.I(e)/(data.L(e)^3)*k0_v;
@@ -202,22 +214,23 @@ for e=(4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)
 
 	nod1 = con(e, 1);
 	nod2 = con(e, 2);
-
-	Kg(2*nod1-2:2*nod1, 2*nod1-2:2*nod1) = Kg(2*nod1-2:2*nod1, 2*nod1-2:2*nod1) + ke(1:3, 1:3); 
-	Kg(2*nod1-2:2*nod1, 2*nod2-2:2*nod2) = Kg(2*nod1-2:2*nod1, 2*nod2-2:2*nod2) + ke(1:3, 4:6); 
-	Kg(2*nod2-2:2*nod2, 2*nod1-2:2*nod1) = Kg(2*nod2-2:2*nod2, 2*nod1-2:2*nod1) + ke(4:6, 1:3); 
-	Kg(2*nod2-2:2*nod2, 2*nod2-2:2*nod2) = Kg(2*nod2-2:2*nod2, 2*nod2-2:2*nod2) + ke(4:6, 4:6); 
+    
+	Kg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) = Kg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) + ke(1:3, 1:3); 
+	Kg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) = Kg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) + ke(1:3, 4:6); 
+	Kg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) = Kg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) + ke(4:6, 1:3); 
+	Kg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) = Kg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) + ke(4:6, 4:6); 
 	
-	Mg(2*nod1-2:2*nod1, 2*nod1-2:2*nod1) = Mg(2*nod1-2:2*nod1, 2*nod1-2:2*nod1) + me(1:3, 1:3); 
-	Mg(2*nod1-2:2*nod1, 2*nod2-2:2*nod2) = Mg(2*nod1-2:2*nod1, 2*nod2-2:2*nod2) + me(1:3, 4:6); 
-	Mg(2*nod2-2:2*nod2, 2*nod1-2:2*nod1) = Mg(2*nod2-2:2*nod2, 2*nod1-2:2*nod1) + me(4:6, 1:3); 
-	Mg(2*nod2-2:2*nod2, 2*nod2-2:2*nod2) = Mg(2*nod2-2:2*nod2, 2*nod2-2:2*nod2) + me(4:6, 4:6); 
+	Mg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) = Mg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) + me(1:3, 1:3); 
+	Mg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) = Mg((2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) + me(1:3, 4:6); 
+	Mg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) = Mg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod1-v_ln) - 2):(2*v_ln + 3*(nod1-v_ln))) + me(4:6, 1:3); 
+	Mg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) = Mg((2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln)), (2*v_ln + 3*(nod2-v_ln) - 2):(2*v_ln + 3*(nod2-v_ln))) + me(4:6, 4:6); 
 
 end
 
 % Passo 6 - Aplicar as condicoes de contorno para as fixacoes (na matriz de rigidez global).
 
 Kgm = Kg; % Matriz de rigidez modificada.
+Mgm = Mg; % Matriz de massa modificada.
 
 % Basicamente zerar as linhas e colunas correspondentes aos
 % nodes fixados. No caso aqui, sao todos os graus de liberdade do
@@ -230,13 +243,42 @@ Kgm(:, (5 + Nod_vh)*2) = 0; Kgm((5 + Nod_vh)*2, :) = 0; Kgm((5 + Nod_vh)*2, (5 +
 
 Kgm(:, (5 + Nod_vh - 1)*2 - 1) = 0; Kgm((5 + Nod_vh - 1)*2 - 1, :) = 0; Kgm((5 + Nod_vh - 1)*2 - 1, (5 + Nod_vh - 1)*2 - 1) = 1;
 
+Mgm(:, (5 + Nod_vh)*2 - 2) = 0; Mgm((5 + Nod_vh)*2 - 2, :) = 0; Mgm((5 + Nod_vh)*2 - 2, (5 + Nod_vh)*2 - 2) = 1;
+Mgm(:, (5 + Nod_vh)*2 - 1) = 0; Mgm((5 + Nod_vh)*2 - 1, :) = 0; Mgm((5 + Nod_vh)*2 - 1, (5 + Nod_vh)*2 - 1) = 1;
+Mgm(:, (5 + Nod_vh)*2) = 0; Mgm((5 + Nod_vh)*2, :) = 0; Mgm((5 + Nod_vh)*2, (5 + Nod_vh)*2) = 1;
+
+Mgm(:, (5 + Nod_vh - 1)*2 - 1) = 0; Mgm((5 + Nod_vh - 1)*2 - 1, :) = 0; Mgm((5 + Nod_vh - 1)*2 - 1, (5 + Nod_vh - 1)*2 - 1) = 1;
+
+
 % Passo 7 - Encontrar os autovalores de [K -(omega^2)*M]
 
-[D, R] = eig(SK, SM)
+Kcc = Kgm;
+Mcc = Mgm;
 
+Kcc(:, (5 + Nod_vh)*2 - 2) = []; Kcc((5 + Nod_vh)*2 - 2, :) = [];
+Kcc(:, (5 + Nod_vh)*2 - 1) = []; Kcc((5 + Nod_vh)*2 - 1, :) = [];
+Kcc(:, (5 + Nod_vh)*2) = []; Kcc((5 + Nod_vh)*2, :) = [];
+
+Kcc(:, (5 + Nod_vh - 1)*2 - 1) = []; Kcc((5 + Nod_vh - 1)*2 - 1, :) = [];
+
+Mcc(:, (5 + Nod_vh)*2 - 2) = []; Mcc((5 + Nod_vh)*2 - 2, :) = []; 
+Mcc(:, (5 + Nod_vh)*2 - 1) = []; Mcc((5 + Nod_vh)*2 - 1, :) = [];
+Mcc(:, (5 + Nod_vh)*2) = []; Mcc((5 + Nod_vh)*2, :) = [];
+
+Mcc(:, (5 + Nod_vh - 1)*2 - 1) = []; Mcc((5 + Nod_vh - 1)*2 - 1, :) = [];
+
+det(Mcc);
+det(Mgm);
+
+%[A, V] = eigs(inv(Mcc)*Kcc, 6, 'smallestabs');
+
+%omega_1 = sqrt(abs(V)/(2*pi));
+
+%{
 % Reescalando os autovetores tal que X'*SM*X=I
 
 for i = 1:2*Nx
 	valuei = D(:, i)' * SM * D(:, i);
 	D(:, i) = D(:, i)/sqrt(valuei);
 end
+%}
