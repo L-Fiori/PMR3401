4983255e952360f2b941d2eca14b8f30784c9568
lucas.fiori <lucas.fiori@usp.br>
2021-07-23 21:23:23 -0300

    Fix item b (working)

--- EP3/main.m
+++ EP3/main.m
@@ -6,7 +6,7 @@ b2 = 0.9; h2 = 0.9;
 area2 = b2*h2;
 D3 = 0.050;
 
-dx = 1;
+dx = 2;
 
 % Passo 1 - Definir a geometria (coordenadas e conectividades).
 
@@ -29,20 +29,24 @@ if (dx == 1)
         (72:88)' (73:89)'];
     
 elseif (dx == 2)
-	Nod_vh = size(0:dx:65, 2) + 1;
-	Nod_p = size(0:dx:23, 2) + 1;
+	Nod_vh = size(8:dx:64, 2) + 6;
+	Nod_p = size(0:dx:22, 2) + 1;
     
-	coords = [(0:dx:65)' 5*ones(Nod_vh-1, 1);
+	coords = [0 5
+              2 5
+              4 5
+              5 5
+              6 5
+              (8:dx:64)' 5*ones(size(8:dx:64, 2), 1);
               65  5;
-              36*ones(Nod_p-1, 1) (0:dx:23)';
-              36 23
-              5 5];
+              36*ones(size(0:dx:22, 2), 1) (0:dx:22)';
+              36 23];
           
-	con = [ 48  47;
-            10  47;
-            34  47;
-            (1:33)' (2:34)';
-            (35:46)' (36:47)'];
+	con = [ 4   48;
+            11  48;
+            35  48;
+            (1:34)' (2:35)';
+            (36:47)' (37:48)'];
 
 elseif (dx == 3)
 	Nod_vh = size(0:dx:65, 2) + 1;
@@ -83,7 +87,7 @@ data.A(4:(4 + Nel_vh - 1)) = area2;
 data.A((4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)) = area1;
 
 data.I(4:(4 + Nel_vh - 1)) = b2*((h2)^3)/12;
-data.I((4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)) = b1*((h1)^3)/12;
+data.I((4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)) = h1*((b1)^3)/12;
 
 data.L = zeros(Nel, 1); % Vetor de comprimento dos elementos.
 data.Q = zeros(Nel, 1); % Vetor de rotacoes dos elementos.
@@ -122,7 +126,7 @@ m0_t = [2 0 0 1 0 0;
         0 0 0 0 0 0;
         0 0 0 0 0 0];
     
-%{
+
 for e=1:3
 
 	% Passo 3 - Construir a matriz de cada elemento.
@@ -157,7 +161,7 @@ for e=1:3
 end
 
 % Construcao das matrizes para os elementos de viga horizontal.
-
+%{
 for e=4:(4 + Nel_vh - 1)
 
 	k0_v = [0  0           0               0  0            0;
@@ -191,13 +195,13 @@ for e=4:(4 + Nel_vh - 1)
 	Mg(3*nod2-2:3*nod2, 3*nod2-2:3*nod2) = Mg(3*nod2-2:3*nod2, 3*nod2-2:3*nod2) + me(4:6, 4:6); 
 
 end
-
-% Construcao das matrizes para os elementos de portico (viga vertical).
 %}
+% Construcao das matrizes para os elementos de portico (viga vertical).
+
         
 %for e=(4 + Nel_vh):(4 + Nel_vh + Nel_p - 1)
 
-for e=1:(4 + Nel_vh + Nel_p - 1)
+for e=4:(4 + Nel_vh + Nel_p - 1)
 	ke_t = (data.E(e)*data.A(e)/data.L(e))*k0_t;
 	me_t = (data.rho(e)*data.A(e)*data.L(e)/6)*m0_t;
 
@@ -269,6 +273,7 @@ Mgm = Mg; % Matriz de massa modificada.
 % node 5 + Nod_vh e w2 do node 5 + Nod_vh - 1 (ultimo node da viga
 % horizontal).
 
+%{
 Kgm(:, (Nod_vh + 1)*3 - 2) = 0; Kgm((Nod_vh + 1)*3 - 2, :) = 0; Kgm((Nod_vh + 1)*3 - 2, (Nod_vh + 1)*3 - 2) = 1;
 Kgm(:, (Nod_vh + 1)*3 - 1) = 0; Kgm((Nod_vh + 1)*3 - 1, :) = 0; Kgm((Nod_vh +1)*3 - 1, (Nod_vh + 1)*3 - 1) = 1;
 Kgm(:, (Nod_vh + 1)*3) = 0; Kgm((Nod_vh + 1)*3, :) = 0; Kgm((Nod_vh + 1)*3, (Nod_vh + 1)*3) = 1;
@@ -298,30 +303,85 @@ Mgm(:, (Nod_vh + 1)*3) = []; Mgm((Nod_vh + 1)*3, :) = [];
 
 Mgm(:, (Nod_vh)*3 - 2) = []; Mgm((Nod_vh)*3 - 2, :) = [];
 Mgm(:, (Nod_vh)*3 - 1) = []; Mgm((Nod_vh)*3 - 1, :) = [];
+%}
 
 i_cc = [(Nod_vh)*3 - 2, (Nod_vh)*3 - 1, (Nod_vh + 1)*3 - 2, (Nod_vh + 1)*3 - 1, (Nod_vh + 1)*3];
 
 indices = []; % indices das colunas nulas - a serem removidas
 
+%{
 for i=1:size(Kgm, 1)
     if Kgm(i, :) == zeros(1, size(Kgm, 2))
         indices(end+1) = i;
     end
 end
+%}
 
-xlswrite('Kgm_zeros.xlsx', Mgm)
+%xlswrite('Kgm_zeros.xlsx', Mgm)
 
-Kgm(~any(Kgm, 2), :) = []; %rows
-Kgm(:, ~any(Kgm, 1)) = []; %columns
+%Kgm(~any(Kgm, 2), :) = []; %rows
+%Kgm(:, ~any(Kgm, 1)) = []; %columns
 
-xlswrite('Kgm.xlsx', Kgm)
+%xlswrite('Kgm.xlsx', Kgm)
 
-xlswrite('Mgm_zeros.xlsx', Mgm)
+%xlswrite('Mgm_zeros.xlsx', Mgm)
 
-Mgm(~any(Mgm, 2), :) = []; %rows
-Mgm(:, ~any(Mgm, 1)) = []; %columns
+%Mgm(~any(Mgm, 2), :) = []; %rows
+%Mgm(:, ~any(Mgm, 1)) = []; %columns
 
-xlswrite('Mgm.xlsx', Mgm)
+%xlswrite('Mgm.xlsx', Mgm)
+
+% Exclui linhas que estão fixadas
+
+list = (1:Ngdl)';
+i_cc = [(Nod_vh)*3 - 2, (Nod_vh)*3 - 1, (Nod_vh + 1)*3 - 2, (Nod_vh + 1)*3 - 1, (Nod_vh + 1)*3];
+
+id_fix = [((Nod_vh)*3 - 2) ((Nod_vh)*3-1) ((Nod_vh+1)*3-2) ((Nod_vh+1)*3-1) ((Nod_vh+1)*3)]';
+id_free = list(ismember(list,id_fix) == 0);
+Kgm = Kg(id_free,id_free);
+Mgm = Mg(id_free,id_free);
+%[A, V] = eigs(Mgm\Kgm, 6, 'smallestabs');
+
+%omega_1 = sqrt(diag(V))/(2*pi)
+
+A = Mgm\Kgm;
+[vec, val] = eigs(A, 6, 'smallestabs'); %smallestabs
+val = sqrt(diag(val))/(2*pi);
+val
+
+if (dx == 2)
+
+    mod = 3
+    U = A(:, mod);
+
+    U = U';
+    for i=2:size(indices, 2)
+        U = [U(1:indices(i)-1) 0 U(indices(i):end)];
+    end
+
+    for i=1:5
+        U = [U(1:i_cc(i)-1) 0 U(i_cc(i):end)];    
+    end
+
+    U = U';
+
+    freq = omega_1(mod);
+    T = 1/freq;
+    dt = T/15;
+    TF = 5*T;
+    t = 0:dt:TF;
+    scale = 10;
+
+    for i=1:length(t)
+        coorExag = coords + scale*[ U(1:3:end) U(2:3:end) ]*sin(2*pi*freq*t(i));
+        plot_struct(coorExag, con, '-r');
+        axis([-1 66 -1 24])
+        pause(0.1);
+        clf;
+    end
+    
+end
+
 
 a0 = 0.1217;
 a1 = 0.0087;
@@ -353,7 +413,7 @@ end
 
 Cgm = a0*Mg2 + a1*Kg2;
 
-va = 1;
+va = 2;
 
 TF = 55/va;
 
@@ -381,7 +441,7 @@ for i=1:length(t)
     
     if (t(i) <= 27/va)
         N1 = 20;
-    elseif 27/va < t(i) <= 47/va
+    elseif t(i) <= 47/va
         N1 = va*t(i) - 7;
     elseif (t(i) > 47/va)
         N1 = 40;
@@ -397,7 +457,7 @@ for i=1:length(t)
     
         if ( t(i) <= (36 - (9 + j))/va )
             V(j, 1) = 0;
-        elseif ( (36 - (9 + j))/va < t(i) <= (36 + 20 -(9 + j))/va )
+        elseif ( t(i) <= (36 + 20 -(9 + j))/va )
             V(j, 1) = -80*9.8*(1 - cos(2*pi*va*t(i)))/2;
         elseif (t(i) > (36 + 20 -(9 + j))/va)
             V(j, 1) = 0;
@@ -420,10 +480,11 @@ for i=1:length(t)
     
     F2 = Fnod;
     
+    %{
     if (i == 1)
         U1 = Kg2\Fnod
     end
-    
+    %}
     
     Feq = F2 - Cgm*( V1 + dt*(1-gamma)*A1 ) - Kg2*( U1 + dt*V1 + dt*dt*0.5*(1 - 2*beta)*A1 );
     A2 = Meq\Feq;
@@ -447,54 +508,13 @@ for i=1:length(t)
     
 end
 
-
-plot(t, UA)
-%hold on
-%plot(t, UB)
-%hold on
-%plot(t, UC)
-%hold on
-%plot(t, UF)
-
-
-
 %{
-[A, V] = eigs(Mgm\Kgm, 7, 'smallestabs');
-
-omega_1 = sqrt(diag(V))/(2*pi)
-
-if (dx == 1)
-
-    mod = 3
-    U = A(:, mod);
-
-    U = U';
-    U = [0 U(1:end)];
-    for i=2:size(indices, 2)
-        U = [U(1:indices(i)-1) 0 U(indices(i):end)];
-    end
-
-    for i=1:5
-        U = [U(1:i_cc(i)-1) 0 U(i_cc(i):end)];    
-    end
-
-    U = U';
-
-    freq = omega_1(mod);
-    T = 1/freq;
-    dt = T/15;
-    TF = 5*T;
-    t = 0:dt:TF;
-    scale = 10;
-
-    for i=1:length(t)
-        coorExag = coords + scale*[ U(1:3:end) U(2:3:end) ]*sin(2*pi*freq*t(i));
-        plot_struct(coorExag, con, '-r');
-        axis([-1 66 -1 24])
-        pause(0.1);
-        clf;
-    end
-    
-end
-
+plot(t, UA)
+hold on
+plot(t, UB)
+hold on
+plot(t, UC)
+hold on
+plot(t, UF)
 %}
+
